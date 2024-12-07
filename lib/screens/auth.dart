650487fe57c244c4c0022220a8fbe5258dbe55c9
Shlomi Lasri <Shlomi087@gmail.com>
2024-12-07 12:37:41 -0800

    added auth screen "mid process"

---
+++ lib/screens/auth.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class AuthScreen extends StatefulWidget {
+  const AuthScreen({super.key});
+
+  @override
+  State<AuthScreen> createState() {
+    return _AuthScreenState();
+  }
+}
+
+class _AuthScreenState extends State<AuthScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.primary,
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                margin: const EdgeInsets.only(
+                  bottom: 20,
+                  top: 30,
+                  left: 20,
+                  right: 20,
+                ),
+                width: 200,
+                child: Image.asset('assets/images/chat.png'),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
